52880cb1570bfe8d1bb2f28960dddd749ba0931f
unknown <o.westhoff@gmx.net>
2021-01-17 10:06:38 +0100

    adjust app.js
    updated commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    next();
+})
 
-server.listen(3000);
+app.listen(3000);
